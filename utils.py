f148e4554fd5493c6ac71fda7d98dec10a0de25d
Agu <aguaguilar.ar@gmail.com>
2018-06-11 13:31:59 -0300

    ok

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def clean_dataframe(df):
 	rename_columns = {
 	    'age_A': 'edad_a', 'age_B': 'edad_b', 'height_A': 'altura_a', 
@@ -20,3 +23,33 @@ def clean_dataframe(df):
 	df = df.drop(["peso_a", "peso_b"], axis=1)
 
 	return df
+
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
